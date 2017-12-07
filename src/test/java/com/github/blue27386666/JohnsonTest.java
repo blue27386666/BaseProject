4fa5bebef22da2c82207ef1cd089e2cf1f565738
Johnson <bvlue27386666@gmail.com>
2017-12-07 19:54:50 +0800

    hello world

---
+++ src/test/java/com/github/blue27386666/JohnsonTest.java
@@ -0,0 +1,20 @@
+package com.github.blue27386666;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JohnsonTest {
+    @Test
+    public void getType() throws Exception {
+        // Arrange
+        Johnson triangle = new  Johnson(5, 10, 5);
+
+        // Act
+        String result = triangle.getType();
+
+        // Assert
+        Assert.assertEquals("不是三角形", result);
+
+}
